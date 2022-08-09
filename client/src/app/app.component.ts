d1639d2dbc341d755a2c44cb5a05bce26fd5aa93
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-09 14:37:01 +0200

    Created get users request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(){
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5149/api/users').subscribe(res => {
+      this.users = res;
+      console.log(res);
+  }, error=> {
+    console.log(error);
+    })
+  }
 }
